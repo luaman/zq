951c620a55e798ef79c43fcedba4a26005a75b4a
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2000-08-26 12:19:34 +0000

    Initial checkin

---
+++ trunk/zquake/net_wins.c
@@ -0,0 +1,430 @@
+/*
+Copyright (C) 1996-1997 Id Software, Inc.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+// net_wins.c
+
+#include "quakedef.h"
+#include "winquake.h"
+
+netadr_t	net_local_adr;
+
+netadr_t	net_from;
+sizebuf_t	net_message;
+int			net_clientsocket;
+int			net_serversocket;
+
+#define	MAX_UDP_PACKET	(MAX_MSGLEN*2)	// one more than msg + header
+byte		net_message_buffer[MAX_UDP_PACKET];
+
+WSADATA		winsockdata;
+
+// Tonik -->
+#define	 PORT_LOOPBACK 65535
+int			loop_c2s_messageLength;
+char		loop_c2s_message[MAX_UDP_PACKET];
+int			loop_s2c_messageLength;
+char		loop_s2c_message[MAX_UDP_PACKET];
+// <-- Tonik
+
+//=============================================================================
+
+void NetadrToSockadr (netadr_t *a, struct sockaddr_in *s)
+{
+	memset (s, 0, sizeof(*s));
+	s->sin_family = AF_INET;
+
+	*(int *)&s->sin_addr = *(int *)&a->ip;
+	s->sin_port = a->port;
+}
+
+void SockadrToNetadr (struct sockaddr_in *s, netadr_t *a)
+{
+	*(int *)&a->ip = *(int *)&s->sin_addr;
+	a->port = s->sin_port;
+}
+
+qboolean	NET_CompareBaseAdr (netadr_t a, netadr_t b)
+{
+	if (a.ip[0] == b.ip[0] && a.ip[1] == b.ip[1] && a.ip[2] == b.ip[2] && a.ip[3] == b.ip[3])
+		return true;
+	return false;
+}
+
+qboolean	NET_CompareAdr (netadr_t a, netadr_t b)
+{
+	if (a.ip[0] == b.ip[0] && a.ip[1] == b.ip[1] && a.ip[2] == b.ip[2] && a.ip[3] == b.ip[3] && a.port == b.port)
+		return true;
+	return false;
+}
+
+char	*NET_AdrToString (netadr_t a)
+{
+	static	char	s[64];
+
+#ifdef QW_BOTH
+	if (*(int *)&a == 0 && a.port == PORT_LOOPBACK)
+		return "loopback";
+#endif
+
+	sprintf (s, "%i.%i.%i.%i:%i", a.ip[0], a.ip[1], a.ip[2], a.ip[3], ntohs(a.port));
+
+	return s;
+}
+
+char	*NET_BaseAdrToString (netadr_t a)
+{
+	static	char	s[64];
+	
+	sprintf (s, "%i.%i.%i.%i", a.ip[0], a.ip[1], a.ip[2], a.ip[3]);
+
+	return s;
+}
+
+/*
+=============
+NET_StringToAdr
+
+idnewt
+idnewt:28000
+192.246.40.70
+192.246.40.70:28000
+=============
+*/
+qboolean	NET_StringToAdr (char *s, netadr_t *a)
+{
+	struct hostent	*h;
+	struct sockaddr_in sadr;
+	char	*colon;
+	char	copy[128];
+
+// Tonik -->	
+#ifdef QW_BOTH
+	if (!strcmp(s, "local"))
+	{
+		memset(a, 0, sizeof(*a));
+		a->port = PORT_LOOPBACK;
+		return true;
+	}
+#endif
+// <-- Tonik
+	
+	memset (&sadr, 0, sizeof(sadr));
+	sadr.sin_family = AF_INET;
+	
+	sadr.sin_port = 0;
+
+	strcpy (copy, s);
+	// strip off a trailing :port if present
+	for (colon = copy ; *colon ; colon++)
+		if (*colon == ':')
+		{
+			*colon = 0;
+			sadr.sin_port = htons((short)atoi(colon+1));	
+		}
+	
+	if (copy[0] >= '0' && copy[0] <= '9')
+	{
+		*(int *)&sadr.sin_addr = inet_addr(copy);
+	}
+	else
+	{
+		if ((h = gethostbyname(copy)) == 0)
+			return 0;
+		*(int *)&sadr.sin_addr = *(int *)h->h_addr_list[0];
+	}
+	
+	SockadrToNetadr (&sadr, a);
+
+	return true;
+}
+
+// Returns true if we can't bind the address locally--in other words, 
+// the IP is NOT one of our interfaces.
+qboolean NET_IsClientLegal(netadr_t *adr)
+{
+#if 0
+	struct sockaddr_in sadr;
+	int newsocket;
+
+	if (adr->ip[0] == 127)
+		return false; // no local connections period
+
+	NetadrToSockadr (adr, &sadr);
+
+	if ((newsocket = socket (PF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+		Sys_Error ("NET_IsClientLegal: socket:", strerror(errno));
+
+	sadr.sin_port = 0;
+
+	if( bind (newsocket, (void *)&sadr, sizeof(sadr)) == -1) 
+	{
+		// It is not a local address
+		close(newsocket);
+		return true;
+	}
+	close(newsocket);
+	return false;
+#else
+	return true;
+#endif
+}
+
+//=============================================================================
+
+qboolean NET_GetPacket (int net_socket)
+{
+	int 	ret;
+	struct sockaddr_in	from;
+	int		fromlen;
+
+// Tonik -->
+	if (net_socket == net_clientsocket && loop_s2c_messageLength > 0)
+	{
+		memcpy (net_message_buffer, loop_s2c_message, loop_s2c_messageLength);
+		net_message.cursize = loop_s2c_messageLength;
+		loop_s2c_messageLength = 0;
+		memset (&from, 0, sizeof(from));
+		from.sin_port = PORT_LOOPBACK;
+		SockadrToNetadr (&from, &net_from);
+		return net_message.cursize;
+	}
+
+	if (net_socket == net_serversocket && loop_c2s_messageLength > 0)
+	{
+//		Con_DPrintf ("NET_GetPacket: c2s\n");
+		memcpy (net_message_buffer, loop_c2s_message, loop_c2s_messageLength);
+		net_message.cursize = loop_c2s_messageLength;
+		loop_c2s_messageLength = 0;
+		memset (&from, 0, sizeof(from));
+		from.sin_port = PORT_LOOPBACK;
+		SockadrToNetadr (&from, &net_from);
+		return net_message.cursize;
+	}
+// <-- Tonik
+
+	fromlen = sizeof(from);
+	ret = recvfrom (net_socket, (char *)net_message_buffer, sizeof(net_message_buffer), 0, (struct sockaddr *)&from, &fromlen);
+	SockadrToNetadr (&from, &net_from);
+
+	if (ret == -1)
+	{
+		int errno = WSAGetLastError();
+
+		if (errno == WSAEWOULDBLOCK)
+			return false;
+		if (errno == WSAEMSGSIZE) {
+			Con_Printf ("Warning:  Oversize packet from %s\n",
+				NET_AdrToString (net_from));
+			return false;
+		}
+
+
+		Sys_Error ("NET_GetPacket: %s", strerror(errno));
+	}
+
+	net_message.cursize = ret;
+	if (ret == sizeof(net_message_buffer) )
+	{
+		Con_Printf ("Oversize packet from %s\n", NET_AdrToString (net_from));
+		return false;
+	}
+
+	return ret;
+}
+
+//=============================================================================
+
+void NET_SendPacket (int net_socket, int length, void *data, netadr_t to)
+{
+	int ret;
+	struct sockaddr_in	addr;
+
+// Tonik -->
+	if (*(int *)&to.ip == 0 && to.port == 65535)	// Loopback
+	{
+		if (net_socket == net_clientsocket)
+		{
+//			if (loop_c2s_messageLength)
+//				Con_Printf ("Warning: NET_SendPacket: loop_c2s: NET_SendPacket without NET_GetPacket\n");
+			memcpy (loop_c2s_message, data, length);
+			loop_c2s_messageLength = length;
+			return;
+		}
+		else if (net_socket == net_serversocket)
+		{
+//			if (loop_s2c_messageLength)
+//				Con_Printf ("Warning: NET_SendPacket: loop_s2c: NET_SendPacket without NET_GetPacket\n");
+			memcpy (loop_s2c_message, data, length);
+			loop_s2c_messageLength = length;
+			return;
+		}
+		Sys_Error("NET_SendPacket: loopback: unknown socket");
+		return;
+	}
+// <-- Tonik
+
+	NetadrToSockadr (&to, &addr);
+
+	ret = sendto (net_socket, data, length, 0, (struct sockaddr *)&addr, sizeof(addr) );
+	if (ret == -1)
+	{
+		int err = WSAGetLastError();
+
+// wouldblock is silent
+        if (err == WSAEWOULDBLOCK)
+	        return;
+
+#ifndef SERVERONLY
+		if (err == WSAEADDRNOTAVAIL)
+			Con_DPrintf("NET_SendPacket Warning: %i\n", err);
+		else
+#endif
+			Con_Printf ("NET_SendPacket ERROR: %i\n", errno);
+	}
+}
+
+//=============================================================================
+
+int UDP_OpenSocket (int port, qboolean crash)
+{
+	int newsocket;
+	struct sockaddr_in address;
+	unsigned long _true = true;
+	int i;
+
+	if ((newsocket = socket (PF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+		Sys_Error ("UDP_OpenSocket: socket:", strerror(errno));
+
+	if (ioctlsocket (newsocket, FIONBIO, &_true) == -1)
+		Sys_Error ("UDP_OpenSocket: ioctl FIONBIO:", strerror(errno));
+
+	address.sin_family = AF_INET;
+//ZOID -- check for interface binding option
+	if ((i = COM_CheckParm("-ip")) != 0 && i < com_argc) {
+		address.sin_addr.s_addr = inet_addr(com_argv[i+1]);
+		Con_Printf("Binding to IP Interface Address of %s\n",
+				inet_ntoa(address.sin_addr));
+	} else
+		address.sin_addr.s_addr = INADDR_ANY;
+
+	if (port == PORT_ANY)
+		address.sin_port = 0;
+	else
+		address.sin_port = htons((short)port);
+	if( bind (newsocket, (void *)&address, sizeof(address)) == -1)
+	{
+#ifdef QW_BOTH
+		if (!crash)
+			return -1;
+		else
+#endif
+		Sys_Error ("UDP_OpenSocket: bind: %s", strerror(errno));
+	}
+
+	return newsocket;
+}
+
+void NET_GetLocalAddress (int net_socket)	// FIXME
+{
+	char	buff[512];
+	struct sockaddr_in	address;
+	int		namelen;
+
+	gethostname(buff, 512);
+	buff[512-1] = 0;
+
+	NET_StringToAdr (buff, &net_local_adr);
+
+	namelen = sizeof(address);
+	if (getsockname (net_socket, (struct sockaddr *)&address, &namelen) == -1)
+		Sys_Error ("NET_Init: getsockname:", strerror(errno));
+	net_local_adr.port = address.sin_port;
+
+	Con_Printf("IP address %s\n", NET_AdrToString (net_local_adr) );
+}
+
+/*
+====================
+NET_Init
+====================
+*/
+int __serverport;	// so we can open it later
+void NET_Init (int clientport, int serverport)
+{
+	WORD	wVersionRequested; 
+	int		r;
+
+	wVersionRequested = MAKEWORD(1, 1); 
+
+	r = WSAStartup (MAKEWORD(1, 1), &winsockdata);
+
+	if (r)
+		Sys_Error ("Winsock initialization failed.");
+
+	//
+	// open the single socket to be used for all communications
+	//
+//	net_socket = UDP_OpenSocket (port);
+	if (clientport)
+		net_clientsocket = UDP_OpenSocket (clientport, true);
+#ifndef QW_BOTH
+	if (serverport)
+		net_serversocket = UDP_OpenSocket (serverport, false);
+#else
+	// An ugly hack to let you run zquake and qwsv or proxy without
+	// changing ports via the command line	-- Tonik, 5 Aug 2000
+	__serverport = serverport;
+	net_serversocket = -1;
+/*	if (net_serversocket == -1)
+	{
+		Con_Printf ("NET_Init: Could not open server socket\n");
+	}	*/
+#endif
+
+	//
+	// init the message buffer
+	//
+	net_message.maxsize = sizeof(net_message_buffer);
+	net_message.data = net_message_buffer;
+
+	//
+	// determine my name & address
+	//
+	if (clientport)
+		NET_GetLocalAddress (net_clientsocket);
+	else if (serverport)
+		NET_GetLocalAddress (net_serversocket);
+
+	Con_Printf("UDP Initialized\n");
+}
+
+/*
+====================
+NET_Shutdown
+====================
+*/
+void	NET_Shutdown (void)
+{
+	if (net_clientsocket)
+		closesocket (net_clientsocket);
+	if (net_serversocket)
+		closesocket (net_serversocket);
+	WSACleanup ();
+}
+
---
+++ trunk/zquake/teamplay.c
@@ -0,0 +1,689 @@
+/*
+	teamplay.c
+
+	Teamplay enhancements ("proxy features")
+
+	Copyright (C) 2000       Anton Gavrilov (tonik@quake.ru)
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to:
+
+		Free Software Foundation, Inc.
+		59 Temple Place - Suite 330
+		Boston, MA  02111-1307, USA
+
+	$Id$
+*/
+
+#include "quakedef.h"
+
+cvar_t _cmd_macros = {"_cmd_macros", "0"};
+cvar_t cl_parsesay = {"cl_parsesay", "0"};
+cvar_t cl_triggers = {"cl_triggers", "0"};
+cvar_t cl_nofake = {"cl_nofake", "0"};
+
+//===========================================================================
+//								TRIGGERS
+//===========================================================================
+
+void *Cmd_FindAlias(s);	 // hmm
+
+void CL_ExecTrigger (char *s)
+{
+	if (!cl_triggers.value)
+		return;
+
+	if (Cmd_FindAlias(s))
+	{
+		Cbuf_AddText (s);
+		Cbuf_AddText ("\n");
+		// maybe call Cbuf_Execute?
+	}
+}
+
+
+int	last_health = 0;
+int	last_items = 0;
+int	last_respawntrigger = 0;
+int	last_deathtrigger = 0;
+
+
+#define	IT_WEAPONS (2|4|8|16|32|64)
+
+void CL_StatChanged (int stat, int value)
+{
+	int		i;
+
+	if (stat == STAT_HEALTH)
+	{
+		if (value > 0) {
+			if (last_health <= 0 /*&& last_health != -999*/)
+			{
+				last_respawntrigger = realtime;
+				CL_ExecTrigger ("f_respawn");
+			}
+			last_health = value;
+			return;
+		}
+		if (last_health > 0) {		// We just died
+			last_deathtrigger = realtime;
+			CL_ExecTrigger ("f_death");
+		}
+		last_health = value;
+	}
+	else if (stat == STAT_ITEMS)
+	{
+		i = value &~ last_items;
+		if (i & IT_WEAPONS && (i & IT_WEAPONS != IT_WEAPONS)
+		|| i & (IT_ARMOR1|IT_ARMOR2|IT_ARMOR3|IT_SUPERHEALTH)
+		|| i & (IT_INVISIBILITY|IT_INVULNERABILITY|IT_SUIT|IT_QUAD))
+		{
+			// ...
+			CL_ExecTrigger ("f_took");
+		}
+		last_items = value;
+	}
+}
+
+void CL_NewMap (void)
+{
+	last_health = 0;
+
+}
+
+
+//===========================================================================
+//								MACROS
+//===========================================================================
+
+
+typedef char *(*mcommand_t) (void);
+
+typedef struct cmd_macro_s
+{
+	struct cmd_macro_s	*next;
+	char	*name;
+	mcommand_t	function;
+} cmd_macro_t;
+
+cmd_macro_t	*cmd_macros;
+
+#define MAX_MACRO_VALUE	255
+
+
+void Cmd_AddMacro (char *macro_name, mcommand_t function)
+{
+	cmd_macro_t	*macro;
+	
+	if (host_initialized)	// because hunk allocation would get stomped
+		Sys_Error ("Cmd_AddMacro after host_initialized");
+		
+// fail if the macro is a variable name
+	if (Cvar_VariableString(macro_name)[0])
+	{
+		Con_Printf ("Cmd_AddMacro: %s already defined as a var\n", macro_name);
+		return;
+	}
+	
+// fail if the macro already exists
+	for (macro=cmd_macros ; macro ; macro=macro->next)
+	{
+		if (!strcmp (macro_name, macro->name))
+		{
+			Con_Printf ("Cmd_AddMacro: %s already defined\n", macro_name);
+			return;
+		}
+	}
+
+	macro = Hunk_Alloc (sizeof(cmd_macro_t));	// FIXME: use zone memory?
+	macro->name = macro_name;
+	macro->function = function;
+	macro->next = cmd_macros;
+	cmd_macros = macro;
+}
+
+
+/*
+==========================================================================
+						 MACRO FUNCTIONS DEFINITION
+==========================================================================
+*/
+
+static char	macro_buf[MAX_MACRO_VALUE];
+
+char *Cmd_Macro_Health_f (void)
+{
+	sprintf(macro_buf, "%i", cl.stats[STAT_HEALTH]);
+	return macro_buf;
+}
+
+char *Cmd_Macro_Armor_f (void)
+{
+	sprintf(macro_buf, "%i", cl.stats[STAT_ARMOR]);
+	return macro_buf;
+}
+
+char *Cmd_Macro_Shells_f (void)
+{
+	sprintf(macro_buf, "%i", cl.stats[STAT_SHELLS]);
+	return macro_buf;
+}
+
+char *Cmd_Macro_Nails_f (void)
+{
+	sprintf(macro_buf, "%i", cl.stats[STAT_NAILS]);
+	return macro_buf;
+}
+
+char *Cmd_Macro_Rockets_f (void)
+{
+	sprintf(macro_buf, "%i", cl.stats[STAT_ROCKETS]);
+	return macro_buf;
+}
+
+char *Cmd_Macro_Cells_f (void)
+{
+	sprintf(macro_buf, "%i", cl.stats[STAT_CELLS]);
+	return macro_buf;
+}
+
+char *Cmd_Macro_Ammo_f (void)
+{
+	sprintf(macro_buf, "%i", cl.stats[STAT_AMMO]);
+	return macro_buf;
+}
+
+char *Cmd_Macro_Weapon_f (void)
+{
+	switch (cl.stats[STAT_ACTIVEWEAPON])
+	{
+	case IT_AXE: return "axe";
+	case IT_SHOTGUN: return "sg";
+	case IT_SUPER_SHOTGUN: return "ssg";
+	case IT_NAILGUN: return "ng";
+	case IT_SUPER_NAILGUN: return "sng";
+	case IT_GRENADE_LAUNCHER: return "gl";
+	case IT_ROCKET_LAUNCHER: return "rl";
+	case IT_LIGHTNING: return "lg";
+	default:
+		return "";
+	}
+}
+
+int	_Cmd_Macro_BestWeapon (void)
+{
+	int	best;
+
+// TODO: calculate best weapon depending on user defined weapon priority
+	
+	best = 0;
+
+	if (cl.stats[STAT_ITEMS] & IT_AXE)
+		best = IT_AXE;
+
+	if (cl.stats[STAT_ITEMS] & IT_SHOTGUN && cl.stats[STAT_SHELLS] >= 1)
+		best = IT_SHOTGUN;
+
+	if (cl.stats[STAT_ITEMS] & IT_SUPER_SHOTGUN && cl.stats[STAT_SHELLS] >= 2)
+		best = IT_SUPER_SHOTGUN;
+
+	if (cl.stats[STAT_ITEMS] & IT_NAILGUN && cl.stats[STAT_NAILS] >= 1)
+		best = IT_NAILGUN;
+
+	if (cl.stats[STAT_ITEMS] & IT_SUPER_NAILGUN && cl.stats[STAT_NAILS] >= 2)
+		best = IT_SUPER_NAILGUN;
+
+	if (cl.stats[STAT_ITEMS] & IT_GRENADE_LAUNCHER && cl.stats[STAT_ROCKETS] >= 1)
+		best = IT_GRENADE_LAUNCHER;
+
+	if (cl.stats[STAT_ITEMS] & IT_LIGHTNING && cl.stats[STAT_CELLS] >= 1)
+		best = IT_LIGHTNING;
+
+	if (cl.stats[STAT_ITEMS] & IT_ROCKET_LAUNCHER && cl.stats[STAT_ROCKETS] >= 1)
+		best = IT_ROCKET_LAUNCHER;
+
+	return best;
+}
+
+char *Cmd_Macro_BestWeapon_f (void)
+{
+	switch (_Cmd_Macro_BestWeapon())
+	{
+	case IT_AXE: return "axe";
+	case IT_SHOTGUN: return "sg";
+	case IT_SUPER_SHOTGUN: return "ssg";
+	case IT_NAILGUN: return "ng";
+	case IT_SUPER_NAILGUN: return "sng";
+	case IT_GRENADE_LAUNCHER: return "gl";
+	case IT_ROCKET_LAUNCHER: return "rl";
+	case IT_LIGHTNING: return "lg";
+	default:
+		return "";
+	}
+}
+
+char *Cmd_Macro_BestAmmo_f (void)
+{
+	switch (_Cmd_Macro_BestWeapon())
+	{
+	case IT_SHOTGUN: case IT_SUPER_SHOTGUN: 
+		sprintf(macro_buf, "%i", cl.stats[STAT_SHELLS]);
+		return macro_buf;
+
+	case IT_NAILGUN: case IT_SUPER_NAILGUN:
+		sprintf(macro_buf, "%i", cl.stats[STAT_NAILS]);
+		return macro_buf;
+
+	case IT_GRENADE_LAUNCHER: case IT_ROCKET_LAUNCHER:
+		sprintf(macro_buf, "%i", cl.stats[STAT_ROCKETS]);
+		return macro_buf;
+
+	case IT_LIGHTNING:
+		sprintf(macro_buf, "%i", cl.stats[STAT_CELLS]);
+		return macro_buf;
+
+	default:
+		return "0";
+	}
+}
+
+char *Cmd_Macro_ArmorType_f (void)
+{
+	if (cl.stats[STAT_ITEMS] & IT_ARMOR1)
+		return "ga";
+	else if (cl.stats[STAT_ITEMS] & IT_ARMOR2)
+		return "ya";
+	else if (cl.stats[STAT_ITEMS] & IT_ARMOR3)
+		return "ra";
+	else
+		return "";	// no armor at all
+}
+
+char *Cmd_Macro_Powerups_f (void)
+{
+	macro_buf[0] = 0;
+
+	if (cl.stats[STAT_ITEMS] & IT_QUAD)
+		strcpy(macro_buf, "quad");
+
+	if (cl.stats[STAT_ITEMS] & IT_INVULNERABILITY)
+	{
+		if (macro_buf[0])
+			strcat(macro_buf, "/");
+		strcat(macro_buf, "pent");
+	}
+
+	if (cl.stats[STAT_ITEMS] & IT_INVISIBILITY)
+	{
+		if (macro_buf[0])
+			strcat(macro_buf, "/");
+		strcat(macro_buf, "ring");
+	}
+
+	return macro_buf;
+}
+
+// Defined later...
+char *Cmd_Macro_Location_f (void);
+
+
+#define MAX_MACRO_STRING 1024
+
+// TODO: rewrite this!
+char *CL_ParseMacroString (char *string)
+{
+	static char	buf[MAX_MACRO_STRING];
+	char	*s;
+	int		i;
+	cmd_macro_t	*macro;
+	char	*macro_string;
+	char	ch;
+
+	if (!_cmd_macros.value && !cl_parsesay.value)
+		return string;
+
+	s = string;
+	i = 0;
+
+	while (*s && i < MAX_MACRO_STRING-1)
+	{
+		if (cl_parsesay.value == 1)
+		{
+			if (*s == '%')
+			{
+				macro_string = NULL;
+				switch (s[1])
+				{
+				case 'a': macro_string = Cmd_Macro_Armor_f(); break;
+				case 'A': macro_string = Cmd_Macro_ArmorType_f(); break;
+				case 'c': macro_string = Cmd_Macro_Cells_f(); break;
+				case 'h': macro_string = Cmd_Macro_Health_f(); break;
+				case 'l': macro_string = Cmd_Macro_Location_f(); break;
+				case 'L': macro_string = Cmd_Macro_Location_f(); break;//FIXME!!!
+				case 'P': macro_string = Cmd_Macro_Powerups_f(); break;
+				case 'r': macro_string = Cmd_Macro_Rockets_f(); break;
+				case 'w': macro_string = Cmd_Macro_Weapon_f(); break;
+				case 'W': macro_string = Cmd_Macro_Ammo_f(); break;
+				default: 
+					buf[i++] = *s++;
+					continue;
+				}
+					if (i + strlen(macro_string) >= MAX_MACRO_STRING-1) // !!! is this right?
+						Sys_Error("CL_ParseMacroString: macro string length > MAX_MACRO_STRING)");
+					strcpy (&buf[i], macro_string);
+					i += strlen(macro_string);
+					s += 2;	// skip % and letter
+					continue;
+			}
+
+			if (*s == '$')
+			{
+				ch = 0;
+				switch (s[1])
+				{
+				case '\\': ch = 13; break;
+				case '[': ch = 0x90; break;
+				case ']': ch = 0x91; break;
+				case 'G': ch = 0x86; break;
+				case 'R': ch = 0x87; break;
+				case 'Y': ch = 0x88; break;
+				case 'B': ch = 0x89; break;
+				}
+
+				if (ch) 
+				{
+					buf[i++] = ch;
+					s += 2;
+					continue;
+				}
+
+			}
+		}
+
+		if (_cmd_macros.value)
+		if (*s == '$')
+		{	
+			s++;
+
+			if (*s == '$')
+			{
+				buf[i++] = '$';
+				s++;
+				continue;
+			}
+
+		// macro search
+			if (_cmd_macros.value)
+			{
+				macro = cmd_macros;
+				while (macro)
+				{
+					if (!strncmp(s, macro->name, strlen(macro->name)))
+					{
+						macro_string = macro->function();
+						if (i + strlen(macro_string) >= MAX_MACRO_STRING-1) // !!! is this right?
+							Sys_Error("CL_ParseMacroString: macro string length > MAX_MACRO_STRING)");
+						strcpy (&buf[i], macro_string);
+						i += strlen(macro_string);
+						s += strlen(macro->name);
+						goto _continue;
+					}
+					macro = macro->next;
+				}
+			}
+
+		// skip unknown macro name
+			while (*s && ((*s >= 'A' && *s <= 'Z') || (*s >= 'a' && *s <= 'z')))
+				s++;	
+			continue;
+		}
+
+		buf[i++] = *s++;
+
+		_continue: ;
+	}
+	buf[i] = 0;
+
+	return	buf;
+}
+
+
+
+void Cmd_Macro_Init (void)
+{
+	Cmd_AddMacro("health", Cmd_Macro_Health_f);
+	Cmd_AddMacro("armortype", Cmd_Macro_ArmorType_f);
+	Cmd_AddMacro("armor", Cmd_Macro_Armor_f);
+	Cmd_AddMacro("shells", Cmd_Macro_Shells_f);
+	Cmd_AddMacro("nails", Cmd_Macro_Nails_f);
+	Cmd_AddMacro("rockets", Cmd_Macro_Rockets_f);
+	Cmd_AddMacro("cells", Cmd_Macro_Cells_f);
+	Cmd_AddMacro("weapon", Cmd_Macro_Weapon_f);
+	Cmd_AddMacro("ammo", Cmd_Macro_Ammo_f);
+	Cmd_AddMacro("bestweapon", Cmd_Macro_BestWeapon_f);
+	Cmd_AddMacro("bestammo", Cmd_Macro_BestAmmo_f);
+	Cmd_AddMacro("powerups", Cmd_Macro_Powerups_f);
+	Cmd_AddMacro("location", Cmd_Macro_Location_f);
+}
+
+
+/*
+=============================================================================
+
+							PROXY .LOC FILES
+
+=============================================================================
+*/
+
+#define MAX_LOC_NAME 16
+
+typedef struct locdata_s {
+	vec3_t coord;
+	char name[MAX_LOC_NAME];
+} locdata_t;
+
+#define MAX_LOC_ENTRIES 400
+
+locdata_t locdata[MAX_LOC_ENTRIES];	// FIXME: allocate dynamically?
+int	loc_numentries;
+
+#define SKIPBLANKS(ptr) while (*ptr == ' ' || *ptr == 9 || *ptr == 13) ptr++
+#define SKIPTOEOL(ptr) while (*ptr != 10 && *ptr == 0) ptr++
+
+void CL_LoadLocFile (char *path, qboolean quiet)
+{
+	char	*buf, *p;
+	int		i, n, sign;
+	int		line;
+	int		nameindex;
+	int		mark;
+	char	locname[MAX_OSPATH];
+
+	if (!*path)
+		return;
+
+	strcpy (locname, "locs/");
+	if (strlen(path) + strlen(locname) + 2+4 > MAX_OSPATH)
+	{
+		Con_Printf ("CL_LoadLocFile: path name > MAX_OSPATH\n");
+		return;
+	}
+	strcat (locname, path);
+	if (!strstr(locname, "."))	
+		strcat (locname, ".loc");	// Add default extension
+
+	mark = Hunk_LowMark ();
+	buf = (char *) COM_LoadHunkFile (locname);
+
+	if (!buf)
+	{
+		if (!quiet)
+			Con_Printf ("Could not load %s\n", locname);
+		return;
+	}
+
+// Parse the whole file now
+
+	loc_numentries = 0;
+
+	p = buf;
+	line = 1;
+
+	while (1)
+	{
+//		while (*buf == ' ' || *buf == 9)
+//			buf++;
+		SKIPBLANKS(p);
+
+		if (*p == 0)
+			goto _endoffile;
+
+		if (*p == 10 || (*p == '/' && p[1] == '/'))
+		{
+			p++;
+			goto _endofline;
+		}
+
+		for (i = 0; i < 3; i++)
+		{
+			n = 0;
+			sign = 1;
+			while (1)
+			{
+				switch (*p++)
+				{
+				case ' ': case 9:
+					goto _next;
+
+				case '-':
+					if (n)
+					{
+						Con_Printf ("Error in loc file on line #%i\n", line);
+						SKIPTOEOL(p);		
+						goto _endofline;
+					}
+					sign = -1;
+					break;
+
+				case '0': case '1': case '2': case '3': case '4':
+				case '5': case '6': case '7': case '8': case '9':
+					n = n*10 + (p[-1] - '0');
+					break;
+
+				default:	// including eol or eof
+					Con_Printf ("Error in loc file on line #%i\n", line);
+					SKIPTOEOL(p);		
+					goto _endofline;
+				}
+			}
+_next:
+			n *= sign;
+			locdata[loc_numentries].coord[i] = n;
+
+			SKIPBLANKS(p);
+		}
+
+
+// Save the location's name
+//
+		nameindex = 0;
+
+		while (1)
+		{
+			switch (*p)
+			{
+			case 13:
+				p++;
+				break;
+
+			case 10: case 0:
+				locdata[loc_numentries].name[nameindex] = 0;
+				loc_numentries++;
+
+				if (loc_numentries >= MAX_LOC_ENTRIES)
+					goto _endoffile;
+
+				// leave the 0 or 10 in buffer, so it is parsed properly
+				goto _endofline;
+
+			default:
+				if (nameindex < MAX_LOC_NAME-1)
+					locdata[loc_numentries].name[nameindex++] = *p;
+				p++;
+			}
+		}
+_endofline:
+		line++;
+	}
+_endoffile:
+
+	Hunk_FreeToLowMark (mark);
+
+	Con_Printf ("Loaded %s (%i locations)\n", locname, loc_numentries);
+}
+
+void CL_LoadLocFile_f (void)
+{
+	if (Cmd_Argc() != 2)
+	{
+		Con_Printf ("loadloc <filename> : load a loc file\n");
+		return;
+	}
+
+	CL_LoadLocFile (Cmd_Argv(1), false);
+}
+
+extern int parsecountmod;
+char *Cmd_Macro_Location_f (void)
+{
+	int		i;
+	int		min_num;
+	vec_t	min_dist;
+	vec3_t	vec;
+	vec3_t	org;
+	
+	if (!loc_numentries || (cls.state != ca_active))
+		return "someplace";
+
+	VectorCopy (cl.frames[parsecountmod].playerstate[cl.playernum].origin, org);
+	for (i = 0; i < 3; i++)
+		org[i] *= 8;
+
+	min_num = 0;
+	min_dist = 9999999;
+
+	for (i = 0; i < loc_numentries; i++)
+	{
+    	Con_DPrintf ("%f %f %f: %s\n", locdata[i].coord[0], locdata[i].coord[1], locdata[i].coord[2], locdata[i].name);
+		VectorSubtract (org, locdata[i].coord, vec);
+		if (Length(vec) < min_dist)
+		{
+			min_num = i;
+			min_dist = Length(vec);
+		}
+	}
+
+	return locdata[min_num].name;
+}
+
+void CL_InitTeamplay()
+{
+	Cvar_RegisterVariable (&_cmd_macros);
+	Cvar_RegisterVariable (&cl_parsesay);
+	Cvar_RegisterVariable (&cl_triggers);
+	Cvar_RegisterVariable (&cl_nofake);
+
+	Cmd_Macro_Init();
+	Cmd_AddCommand ("loadloc", CL_LoadLocFile_f);
+}
