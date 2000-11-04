83520cd3b3bcbbd02c015596a98fa8edd1c140d2
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2000-11-04 21:21:55 +0000

    Increase version number from 0.098 to 0.099pre

--- trunk/zquake/version.h
+++ trunk/zquake/version.h
@@ -2,11 +2,11 @@
 
 #define GLQUAKE_VERSION 1.00
 #define	VERSION		2.40
-#define Z_VERSION	"0.098"
+#define Z_VERSION	"0.099pre"
 #define LINUX_VERSION 0.98
 
 //#define TEST_VERSION
-#define RELEASE_VERSION
+//#define RELEASE_VERSION
 
 int build_number (void);
 void CL_Version_f (void);
