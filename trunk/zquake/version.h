e4716ee97b16086443ecfcfd19304eddb774a4e6
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2001-02-04 03:02:18 +0000

    Z_VERSION is 0.10 now!

--- trunk/zquake/version.h
+++ trunk/zquake/version.h
@@ -2,11 +2,11 @@
 
 #define GLQUAKE_VERSION 1.00
 #define	VERSION		2.40
-#define Z_VERSION	"0.099pre"
+#define Z_VERSION	"0.10"
 #define LINUX_VERSION 0.98
 
 //#define TEST_VERSION
-//#define RELEASE_VERSION
+#define RELEASE_VERSION
 
 int build_number (void);
 void CL_Version_f (void);
