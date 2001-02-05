c2a8d4db843de65f1cea5e28ac96b72a8a7fef5a
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2001-02-05 00:27:40 +0000

    1. Rename VERSION to QW_VERSION 2. Remove TEST_VERSION 3. Z_VERSION is now 0.11dev

--- zquake/version.h
+++ zquake/version.h
@@ -1,12 +1,11 @@
 // version.h
 
 #define GLQUAKE_VERSION 1.00
-#define	VERSION		2.40
-#define Z_VERSION	"0.10"
+#define	QW_VERSION		2.40
+#define Z_VERSION	"0.11dev"
 #define LINUX_VERSION 0.98
 
-//#define TEST_VERSION
-#define RELEASE_VERSION
+//#define RELEASE_VERSION
 
 int build_number (void);
 void CL_Version_f (void);
