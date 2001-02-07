4143a3355aa89df103d1318df107bc252aada1a8
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2001-02-07 16:57:45 +0000

    GL waterwarp effect was removed

--- trunk/zquake/gl_rmisc.c
+++ trunk/zquake/gl_rmisc.c
@@ -188,7 +188,6 @@ void R_Init (void)
 	Cvar_RegisterVariable (&r_shadows);
 	Cvar_RegisterVariable (&r_mirroralpha);
 	Cvar_RegisterVariable (&r_wateralpha);
-	Cvar_RegisterVariable (&r_waterwarp);
 	Cvar_RegisterVariable (&r_dynamic);
 	Cvar_RegisterVariable (&r_novis);
 	Cvar_RegisterVariable (&r_speeds);
