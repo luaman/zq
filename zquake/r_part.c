afcdbdfc8fc3d45ab70eb909c569314e91c00a49
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2001-05-31 19:31:06 +0000

    In R_DrawParticles() (GLQUAKE): 1) Disable z-buffer writes 2) Fov correction for particle size scale

--- zquake/r_part.c
+++ zquake/r_part.c
@@ -459,15 +459,20 @@ void R_DrawParticles (void)
 	unsigned char	*at;
 	unsigned char	theAlpha;
 	vec3_t			up, right;
-	float			scale;
+	float			dist, scale;
 	qboolean		alphaTestEnabled;
-    
+    float			scalefactor;
+
+	// FIXME: precalc this
+	scalefactor = 0.004 * tan (r_refdef.fov_x * (M_PI/180) / 2);
+
 	GL_Bind(particletexture);
 	alphaTestEnabled = glIsEnabled(GL_ALPHA_TEST);
 	
 	if (alphaTestEnabled)
 		glDisable(GL_ALPHA_TEST);
 	glEnable (GL_BLEND);
+	glDepthMask (GL_FALSE);
 	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 	glBegin (GL_TRIANGLES);
 
@@ -482,6 +487,8 @@ void R_DrawParticles (void)
 #endif
 
 	frametime = host_frametime;
+	if (cl.paused)
+		frametime = 0;
 	time3 = frametime * 15;
 	time2 = frametime * 10; // 15;
 	time1 = frametime * 5;
@@ -518,12 +525,10 @@ void R_DrawParticles (void)
 
 #ifdef GLQUAKE
 		// hack a scale up to keep particles from disapearing
-		scale = (p->org[0] - r_origin[0])*vpn[0] + (p->org[1] - r_origin[1])*vpn[1]
+		dist = (p->org[0] - r_origin[0])*vpn[0] + (p->org[1] - r_origin[1])*vpn[1]
 			+ (p->org[2] - r_origin[2])*vpn[2];
-		if (scale < 20)
-			scale = 1;
-		else
-			scale = 1 + scale * 0.004;
+		scale = 1 + dist * scalefactor;
+
 		at = (byte *)&d_8to24table[(int)p->color];
 		if (p->type==pt_fire)
 			theAlpha = 255*(6-p->ramp)/6;
@@ -533,8 +538,6 @@ void R_DrawParticles (void)
 		else
 			theAlpha = 255;
 		glColor4ub (*at, *(at+1), *(at+2), theAlpha);
-//		glColor3ubv (at);
-//		glColor3ubv ((byte *)&d_8to24table[(int)p->color]);
 		glTexCoord2f (0,0);
 		glVertex3fv (p->org);
 		glTexCoord2f (1,0);
@@ -607,6 +610,7 @@ void R_DrawParticles (void)
 #ifdef GLQUAKE
 	glEnd ();
 	glDisable (GL_BLEND);
+	glDepthMask (GL_TRUE);
 	if (alphaTestEnabled)
 		glEnable(GL_ALPHA_TEST);
 	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
