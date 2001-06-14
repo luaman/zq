7b3fda6e14465ea0dea20452b3d53e771916b69f
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2001-06-14 21:42:41 +0000

    Two new "fun char" combinations - ctrl+shift+B & ctrl+shift+C -- filled red block and red arrow (pointing right)

--- zquake/keys.c
+++ zquake/keys.c
@@ -431,25 +431,25 @@ no_lf:
 
 	if (keydown[K_CTRL]) {
 		if (key >= '0' && key <= '9')
-			key = key - '0' + 0x12;	// yellow number
-		else switch (key)
-		{
-		case '[': key = 0x10; break;
-		case ']': key = 0x11; break;
-		case 'g': key = 0x86; break;
-		case 'r': key = 0x87; break;
-		case 'y': key = 0x88; break;
-		case 'b': key = 0x89; break;
-		case '(': key = 0x80; break;
-		case '=': key = 0x81; break;
-		case ')': key = 0x82; break;
-		case 'a': key = 0x83; break;
-		case '<': key = 0x1d; break;
-		case '-': key = 0x1e; break;
-		case '>': key = 0x1f; break;
-		case ',': key = 0x1c; break;
-		case '.': key = 0x9c; break;
-		//case '?': key = 0x8b; break;	// filled red block
+				key = key - '0' + 0x12;	// yellow number
+		else switch (key) {
+			case '[': key = 0x10; break;
+			case ']': key = 0x11; break;
+			case 'g': key = 0x86; break;
+			case 'r': key = 0x87; break;
+			case 'y': key = 0x88; break;
+			case 'b': key = 0x89; break;
+			case '(': key = 0x80; break;
+			case '=': key = 0x81; break;
+			case ')': key = 0x82; break;
+			case 'a': key = 0x83; break;
+			case '<': key = 0x1d; break;
+			case '-': key = 0x1e; break;
+			case '>': key = 0x1f; break;
+			case ',': key = 0x1c; break;
+			case '.': key = 0x9c; break;
+			case 'B': key = 0x8b; break;
+			case 'C': key = 0x8d; break;
 		}
 	}
 
