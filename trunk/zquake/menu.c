ff9193061927a794672a729f10f17f01ab010580
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2001-09-15 21:18:04 +0000

    Add #include "input.h"

--- trunk/zquake/menu.c
+++ trunk/zquake/menu.c
@@ -20,6 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "quakedef.h"
 #include "winquake.h"
 #include "cl_slist.h"
+#include "input.h"
 #include "keys.h"
 #include "sound.h"
 #include "version.h"
