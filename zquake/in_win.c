ddb793f6f5bf69fdb243517e410518fa7cb3e497
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2001-09-15 21:18:04 +0000

    Add #include "input.h"

--- zquake/in_win.c
+++ zquake/in_win.c
@@ -23,6 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <dinput.h>
 #include "quakedef.h"
 #include "winquake.h"
+#include "input.h"
 #include "keys.h"
 
 #define DINPUT_BUFFERSIZE           16
