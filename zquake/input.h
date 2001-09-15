820f9bdf5235c7bbb4d11c460913d52f576db0fe
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2001-09-15 22:05:38 +0000

    A number defs were moved here from client.h

--- zquake/input.h
+++ zquake/input.h
@@ -32,3 +32,41 @@ void IN_Move (usercmd_t *cmd);
 void IN_ModeChanged (void);
 // called whenever screen dimensions change
 
+//
+// cl_input.c
+//
+typedef struct
+{
+	int		down[2];		// key nums holding it down
+	int		state;			// low bit is down state
+} kbutton_t;
+
+extern kbutton_t	in_mlook, in_klook;
+extern kbutton_t 	in_strafe;
+extern kbutton_t 	in_speed;
+
+void CL_InitInput (void);
+void CL_SendCmd (void);
+void CL_BaseMove (usercmd_t *cmd);
+float CL_KeyState (kbutton_t *key);
+
+
+extern cvar_t	cl_upspeed;
+extern cvar_t	cl_forwardspeed;
+extern cvar_t	cl_backspeed;
+extern cvar_t	cl_sidespeed;
+extern cvar_t	cl_movespeedkey;
+extern cvar_t	cl_anglespeedkey;
+extern cvar_t	cl_yawspeed;
+extern cvar_t	cl_pitchspeed;
+
+extern cvar_t	sensitivity;
+extern cvar_t	lookspring;
+extern cvar_t	lookstrafe;
+
+extern cvar_t	m_pitch;
+extern cvar_t	m_yaw;
+extern cvar_t	m_forward;
+extern cvar_t	m_side;
+
+extern cvar_t	_windowed_mouse;
