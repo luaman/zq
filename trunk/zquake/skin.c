229d1ca7919d7c4cadd3c0b91a1a776baf975e4a
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2001-10-31 17:59:03 +0000

    Add #include "image.h"

--- trunk/zquake/skin.c
+++ trunk/zquake/skin.c
@@ -19,6 +19,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include "quakedef.h"
+#include "image.h"
 #include "teamplay.h"
 
 cvar_t		baseskin = {"baseskin", "base"};
