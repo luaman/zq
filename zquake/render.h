a983b1b88122c72c6073119d06a2a1418f74415b
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2001-10-31 17:58:39 +0000

    pcx_t definition was moved to image

--- zquake/render.h
+++ zquake/render.h
@@ -152,20 +152,3 @@ void D_InitCaches (void *buffer, int size);
 void R_SetVrect (vrect_t *pvrect, vrect_t *pvrectin, int lineadj);
 
 extern float r_partscale;
-
-typedef struct
-{
-    char	manufacturer;
-    char	version;
-    char	encoding;
-    char	bits_per_pixel;
-    unsigned short	xmin,ymin,xmax,ymax;
-    unsigned short	hres,vres;
-    unsigned char	palette[48];
-    char	reserved;
-    char	color_planes;
-    unsigned short	bytes_per_line;
-    unsigned short	palette_type;
-    char	filler[58];
-    unsigned char	data;			// unbounded
-} pcx_t;
