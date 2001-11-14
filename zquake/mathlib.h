e503f438d48518cffbeccb84e946ee3b79b41618
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2001-11-14 12:50:35 +0000

    1) Add VectorNegate & VectorSet macros. 2) Re-format other macros to Q2 style - (foo,bar) instead of {foo;bar;}

--- zquake/mathlib.h
+++ zquake/mathlib.h
@@ -43,11 +43,13 @@ extern vec3_t vec3_origin;
 #define NANMASK (255<<23)
 #define	IS_NAN(x) (((*(int *)&x)&NANMASK)==NANMASK)
 
-#define DotProduct(x,y) (x[0]*y[0]+x[1]*y[1]+x[2]*y[2])
-#define VectorSubtract(a,b,c) {c[0]=a[0]-b[0];c[1]=a[1]-b[1];c[2]=a[2]-b[2];}
-#define VectorAdd(a,b,c) {c[0]=a[0]+b[0];c[1]=a[1]+b[1];c[2]=a[2]+b[2];}
-#define VectorCopy(a,b) {b[0]=a[0];b[1]=a[1];b[2]=a[2];}
-#define VectorClear(a) {a[0]=a[1]=a[2]=0;}
+#define DotProduct(x,y)			(x[0]*y[0]+x[1]*y[1]+x[2]*y[2])
+#define VectorSubtract(a,b,c)	(c[0]=a[0]-b[0],c[1]=a[1]-b[1],c[2]=a[2]-b[2])
+#define VectorAdd(a,b,c)		(c[0]=a[0]+b[0],c[1]=a[1]+b[1],c[2]=a[2]+b[2])
+#define VectorCopy(a,b)			(b[0]=a[0],b[1]=a[1],b[2]=a[2])
+#define VectorClear(a)			(a[0]=a[1]=a[2]=0)
+#define VectorNegate(a,b)		(a[0]=-a[0],a[1]=-a[1],a[2]=-a[2])
+#define VectorSet(v, x, y, z)	(v[0]=(x),v[1]=(y),v[2]=(z))
 
 void VectorMA (vec3_t veca, float scale, vec3_t vecb, vec3_t vecc);
 
