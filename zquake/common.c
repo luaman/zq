d4a2c800ee18850ce96d5a7e3167d3c768fc8a7e
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2001-12-12 17:47:06 +0000

    LordHavoc's MSG_WriteAngle fix is evil because it doesn't round negative values properly. Q_rint macro is now used instead of blindly adding 0.5

--- zquake/common.c
+++ zquake/common.c
@@ -379,12 +379,12 @@ void MSG_WriteCoord (sizebuf_t *sb, float f)
 
 void MSG_WriteAngle (sizebuf_t *sb, float f)
 {
-	MSG_WriteByte (sb, (int)(f*(256.0f/360.0f) + 0.5f) & 255);
+	MSG_WriteByte (sb, Q_rint(f*256.0/360.0) & 255);
 }
 
 void MSG_WriteAngle16 (sizebuf_t *sb, float f)
 {
-	MSG_WriteShort (sb, (int)(f*(65536.0f/360.0f) + 0.5f) & 65535);
+	MSG_WriteShort (sb, Q_rint(f*65536.0/360.0) & 65535);
 }
 
 void MSG_WriteDeltaUsercmd (sizebuf_t *buf, usercmd_t *from, usercmd_t *cmd)
