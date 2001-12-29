af43f1e4b8cc37f53290e72e8d59cec3d83bc36e
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2001-12-29 16:21:33 +0000

    modellist, soundlist and prespawn 'names' are no longer encrypted as it doesn't make much sense for an open source project. However, emodel and pmodel encryption is still there, just to prevent llamas from hacking them.

--- trunk/zquake/client.h
+++ trunk/zquake/client.h
@@ -370,7 +370,7 @@ extern	int				cl_numvisedicts, cl_oldnumvisedicts;
 extern	entity_t		*cl_visedicts, *cl_oldvisedicts;
 extern	entity_t		cl_visedicts_list[2][MAX_VISEDICTS];
 
-extern char emodel_name[], pmodel_name[], prespawn_name[], modellist_name[], soundlist_name[];
+extern char emodel_name[], pmodel_name[];
 
 
 //
