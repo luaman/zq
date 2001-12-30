0d1ef8bcddf65d1b1f22b05ee5cdf86eca2fa409
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2001-12-30 22:00:41 +0000

    #include cdaudio.h to get rid of compiler warnings

--- zquake/cd_linux.c
+++ zquake/cd_linux.c
@@ -17,8 +17,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
-// Quake is a trademark of Id Software, Inc., (c) 1996 Id Software, Inc. All
-// rights reserved.
+// cd_linux.c
 
 #include <stdio.h>
 #include <unistd.h>
@@ -33,6 +32,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <linux/cdrom.h>
 
 #include "quakedef.h"
+#include "cdaudio.h"
 #include "sound.h"
 
 static qboolean cdValid = false;
