47dbb8688b985c2e61ac23a842d9f2199005b1a7
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2001-12-30 21:59:00 +0000

    Apply Quake2 style networking changes

--- zquake/net_udp.c
+++ zquake/net_udp.c
@@ -17,12 +17,13 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
-// net_main.c
+// net_udp.c
 
 #include "quakedef.h"
-
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <unistd.h>
+#include <sys/time.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <sys/param.h>
@@ -30,10 +31,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <sys/uio.h>
 #include <arpa/inet.h>
 #include <errno.h>
-
-#if defined(sun)
-#include <unistd.h>
-#endif
 
 #ifdef sun
 #include <sys/filio.h>
@@ -47,17 +44,26 @@ netadr_t	net_local_adr;
 
 netadr_t	net_from;
 sizebuf_t	net_message;
-int			net_socket;			// non blocking, for receives
-int			net_send_socket;	// blocking, for sends
 
-int		net_clientsocket;	// @@@ dummy
-int		net_serversocket;	// @@@ dummy
-
-#define	MAX_UDP_PACKET	8192
+#define	MAX_UDP_PACKET	(MAX_MSGLEN*2)	// one more than msg + header
 byte		net_message_buffer[MAX_UDP_PACKET];
 
-int gethostname (char *, int);
-int close (int);
+#define	MAX_LOOPBACK	4	// must be a power of two
+
+typedef struct
+{
+	byte	data[MAX_UDP_PACKET];
+	int		datalen;
+} loopmsg_t;
+
+typedef struct
+{
+	loopmsg_t	msgs[MAX_LOOPBACK];
+	unsigned int	get, send;
+} loopback_t;
+
+loopback_t	loopbacks[2];
+int			ip_sockets[2] = { -1, -1 };
 
 //=============================================================================
 
@@ -72,20 +78,24 @@ void NetadrToSockadr (netadr_t *a, struct sockaddr_in *s)
 
 void SockadrToNetadr (struct sockaddr_in *s, netadr_t *a)
 {
+	a->type = NA_IP;
 	*(int *)&a->ip = *(int *)&s->sin_addr;
 	a->port = s->sin_port;
 }
 
 qboolean	NET_CompareBaseAdr (netadr_t a, netadr_t b)
 {
+	if (a.type == NA_LOOPBACK && b.type == NA_LOOPBACK)
+		return true;
 	if (a.ip[0] == b.ip[0] && a.ip[1] == b.ip[1] && a.ip[2] == b.ip[2] && a.ip[3] == b.ip[3])
 		return true;
 	return false;
 }
 
-
 qboolean	NET_CompareAdr (netadr_t a, netadr_t b)
 {
+	if (a.type == NA_LOOPBACK && b.type == NA_LOOPBACK)
+		return true;
 	if (a.ip[0] == b.ip[0] && a.ip[1] == b.ip[1] && a.ip[2] == b.ip[2] && a.ip[3] == b.ip[3] && a.port == b.port)
 		return true;
 	return false;
@@ -94,7 +104,10 @@ qboolean	NET_CompareAdr (netadr_t a, netadr_t b)
 char	*NET_AdrToString (netadr_t a)
 {
 	static	char	s[64];
-	
+
+	if (a.type == NA_LOOPBACK)
+		return "loopback";
+
 	sprintf (s, "%i.%i.%i.%i:%i", a.ip[0], a.ip[1], a.ip[2], a.ip[3], ntohs(a.port));
 
 	return s;
@@ -119,13 +132,19 @@ idnewt:28000
 192.246.40.70:28000
 =============
 */
-qboolean	NET_StringToAdr (char *s, netadr_t *a)
+qboolean NET_StringToAdr (char *s, netadr_t *a)
 {
 	struct hostent	*h;
 	struct sockaddr_in sadr;
 	char	*colon;
 	char	copy[128];
-	
+
+	if (!strcmp(s, "local"))
+	{
+		memset(a, 0, sizeof(*a));
+		a->type = NA_LOOPBACK;
+		return true;
+	}
 	
 	memset (&sadr, 0, sizeof(sadr));
 	sadr.sin_family = AF_INET;
@@ -138,7 +157,7 @@ qboolean	NET_StringToAdr (char *s, netadr_t *a)
 		if (*colon == ':')
 		{
 			*colon = 0;
-			sadr.sin_port = htons(atoi(colon+1));	
+			sadr.sin_port = htons((short)atoi(colon+1));	
 		}
 	
 	if (copy[0] >= '0' && copy[0] <= '9')
@@ -147,7 +166,7 @@ qboolean	NET_StringToAdr (char *s, netadr_t *a)
 	}
 	else
 	{
-		if (! (h = gethostbyname(copy)) )
+		if ((h = gethostbyname(copy)) == 0)
 			return 0;
 		*(int *)&sadr.sin_addr = *(int *)h->h_addr_list[0];
 	}
@@ -158,13 +177,71 @@ qboolean	NET_StringToAdr (char *s, netadr_t *a)
 }
 
 
+/*
+=============================================================================
+
+LOOPBACK BUFFERS FOR LOCAL PLAYER
+
+=============================================================================
+*/
+
+qboolean NET_GetLoopPacket (netsrc_t sock)
+{
+	int		i;
+	loopback_t	*loop;
+
+	loop = &loopbacks[sock];
+
+	if (loop->send - loop->get > MAX_LOOPBACK)
+		loop->get = loop->send - MAX_LOOPBACK;
+
+	if ((int)(loop->send - loop->get) <= 0)
+		return false;
+
+	i = loop->get & (MAX_LOOPBACK-1);
+	loop->get++;
+
+	memcpy (net_message.data, loop->msgs[i].data, loop->msgs[i].datalen);
+	net_message.cursize = loop->msgs[i].datalen;
+	memset (&net_from, 0, sizeof(net_from));
+	net_from.type = NA_LOOPBACK;
+	return true;
+
+}
+
+
+void NET_SendLoopPacket (netsrc_t sock, int length, void *data, netadr_t to)
+{
+	int		i;
+	loopback_t	*loop;
+
+	loop = &loopbacks[sock^1];
+
+	i = loop->send & (MAX_LOOPBACK-1);
+	loop->send++;
+
+	if (length > sizeof(loop->msgs[i].data))
+		Sys_Error ("NET_SendLoopPacket: length > MAX_UDP_PACKET");
+
+	memcpy (loop->msgs[i].data, data, length);
+	loop->msgs[i].datalen = length;
+}
+
 //=============================================================================
 
-qboolean NET_GetPacket (int dummy)
+qboolean NET_GetPacket (netsrc_t sock)
 {
 	int 	ret;
 	struct sockaddr_in	from;
 	int		fromlen;
+	int		net_socket;
+
+	if (NET_GetLoopPacket (sock))
+		return true;
+
+	net_socket = ip_sockets[sock];
+	if (net_socket == -1)
+		return false;
 
 	fromlen = sizeof(from);
 	ret = recvfrom (net_socket, net_message_buffer, sizeof(net_message_buffer), 0, (struct sockaddr *)&from, &fromlen);
@@ -185,10 +262,21 @@ qboolean NET_GetPacket (int dummy)
 
 //=============================================================================
 
-void NET_SendPacket (int dummy, int length, void *data, netadr_t to)
+void NET_SendPacket (netsrc_t sock, int length, void *data, netadr_t to)
 {
-	int ret;
+	int		ret;
 	struct sockaddr_in	addr;
+	int		net_socket;
+
+	if (to.type == NA_LOOPBACK)
+	{
+		NET_SendLoopPacket (sock, length, data, to);
+		return;
+	}
+
+	net_socket = ip_sockets[sock];
+	if (net_socket == -1)
+		return;
 
 	NetadrToSockadr (&to, &addr);
 
@@ -206,79 +294,144 @@ void NET_SendPacket (int dummy, int length, void *data, netadr_t to)
 
 int UDP_OpenSocket (int port)
 {
-	int newsocket;
-	struct sockaddr_in address;
+	int			newsocket;
+	struct		sockaddr_in address;
 	qboolean _true = true;
 	int i;
 
 	if ((newsocket = socket (PF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
 		Sys_Error ("UDP_OpenSocket: socket:", strerror(errno));
+
 	if (ioctl (newsocket, FIONBIO, (char *)&_true) == -1)
 		Sys_Error ("UDP_OpenSocket: ioctl FIONBIO:", strerror(errno));
+
 	address.sin_family = AF_INET;
-//ZOID -- check for interface binding option
+
+// check for interface binding option
 	if ((i = COM_CheckParm("-ip")) != 0 && i < com_argc) {
 		address.sin_addr.s_addr = inet_addr(com_argv[i+1]);
-		Con_Printf("Binding to IP Interface Address of %s\n",
+		Con_Printf ("Binding to IP Interface Address of %s\n",
 				inet_ntoa(address.sin_addr));
 	} else
 		address.sin_addr.s_addr = INADDR_ANY;
+
 	if (port == PORT_ANY)
 		address.sin_port = 0;
 	else
 		address.sin_port = htons((short)port);
-	if( bind (newsocket, (void *)&address, sizeof(address)) == -1)
-		Sys_Error ("UDP_OpenSocket: bind: %s", strerror(errno));
+
+	if (bind (newsocket, (void *)&address, sizeof(address)) == -1)
+		return -1;
 
 	return newsocket;
 }
 
-void NET_GetLocalAddress (void)
+
+/*
+====================
+NET_Config
+====================
+*/
+void NET_Config (qboolean client, qboolean server)
 {
-	char	buff[MAXHOSTNAMELEN];
-	struct sockaddr_in	address;
-	int		namelen;
+#if defined(QW_BOTH) || defined(SERVERONLY)
+	int		i, port;
+#endif
 
-	gethostname(buff, MAXHOSTNAMELEN);
-	buff[MAXHOSTNAMELEN-1] = 0;
+#ifndef SERVERONLY
+	if (client)
+	{
+		if (ip_sockets[NS_CLIENT] == -1)
+		{
+			ip_sockets[NS_CLIENT] = UDP_OpenSocket (PORT_CLIENT);
+			if (ip_sockets[NS_CLIENT] == -1)
+				Sys_Error ("Couldn't allocate client socket");
+		}
+	}
+	else
+	{
+		if (ip_sockets[NS_CLIENT] != -1) {
+			close (ip_sockets[NS_CLIENT]);
+			ip_sockets[NS_CLIENT] = -1;
+		}
+	}
+#endif
 
-	NET_StringToAdr (buff, &net_local_adr);
+#if defined(QW_BOTH) || defined(SERVERONLY)
+	if (server)
+	{
+		if (ip_sockets[NS_SERVER] != -1)
+			return;
 
-	namelen = sizeof(address);
-	if (getsockname (net_socket, (struct sockaddr *)&address, &namelen) == -1)
-		Sys_Error ("NET_Init: getsockname:", strerror(errno));
-	net_local_adr.port = address.sin_port;
+		port = 0;
+		i = COM_CheckParm ("-port");
+		if (i && i < com_argc)
+			port = atoi(com_argv[i+1]);
+		if (!port)
+			port = PORT_SERVER;
 
-	Con_Printf("IP address %s\n", NET_AdrToString (net_local_adr) );
+		ip_sockets[NS_SERVER] = UDP_OpenSocket (port);
+		if (ip_sockets[NS_SERVER] == -1)
+		{
+#ifdef SERVERONLY
+			Sys_Error ("Couldn't allocate server socket");
+#else
+			Con_Printf ("WARNING: Couldn't allocate server socket.\n");
+#endif
+		}
+	}
+	else
+	{
+		if (ip_sockets[NS_SERVER] != -1) {
+			close (ip_sockets[NS_SERVER]);
+			ip_sockets[NS_SERVER] = -1;
+		}
+	}
+#endif
 }
+
+
+/*
+====================
+NET_Sleep
+
+Sleeps msec or until the server socket is ready
+====================
+*/
+void NET_Sleep (int msec)
+{
+#ifdef SERVERONLY
+    struct timeval timeout;
+	fd_set	fdset;
+	extern qboolean do_stdin, stdin_ready;
+
+	if (ip_sockets[NS_SERVER] == -1)
+		return; // we're not a server, just run full speed
+
+	FD_ZERO (&fdset);
+	if (do_stdin)
+		FD_SET (0, &fdset); // stdin is processed too
+	FD_SET (ip_sockets[NS_SERVER], &fdset); // network socket
+	timeout.tv_sec = msec/1000;
+	timeout.tv_usec = (msec%1000)*1000;
+	select (ip_sockets[NS_SERVER]+1, &fdset, NULL, NULL, &timeout);
+	stdin_ready = FD_ISSET (0, &fdset);
+#endif
+}
+
 
 /*
 ====================
 NET_Init
 ====================
 */
-void NET_Init (int port, int dummy)
+void NET_Init (void)
 {
-	if (dummy)
-		port = dummy;
-
-	//
-	// open the single socket to be used for all communications
-	//
-	net_socket = UDP_OpenSocket (port);
-
 	//
 	// init the message buffer
 	//
 	net_message.maxsize = sizeof(net_message_buffer);
 	net_message.data = net_message_buffer;
-
-	//
-	// determine my name & address
-	//
-	NET_GetLocalAddress ();
-
-	Con_Printf("UDP Initialized\n");
 }
 
 /*
@@ -286,8 +439,7 @@ void NET_Init (int port, int dummy)
 NET_Shutdown
 ====================
 */
-void	NET_Shutdown (void)
+void NET_Shutdown (void)
 {
-	close (net_socket);
+	NET_Config (false, false);
 }
-
