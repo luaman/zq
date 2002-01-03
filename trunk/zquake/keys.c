fa0cc71d9f7f7a31b6a1f486591ca69b18666b7f
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2002-01-03 11:52:52 +0000

    Con_NotifyBox was removed, so we don't need key_count any more

--- trunk/zquake/keys.c
+++ trunk/zquake/keys.c
@@ -40,8 +40,6 @@ int		history_line=0;
 
 keydest_t	key_dest;
 
-int		key_count;			// incremented every key event
-
 char	*keybindings[256];
 qboolean	consolekeys[256];	// if true, can't be rebound while in console
 qboolean	menubound[256];	// if true, can't be rebound while in menu
@@ -1034,11 +1032,6 @@ void Key_Event (int key, qboolean down)
 		key_repeats[key] = 0;
 
 	key_lastpress = key;
-	key_count++;
-	if (key_count <= 0)
-	{
-		return;		// just catching keys for Con_NotifyBox
-	}
 
 // update auto-repeat status
 	if (down)
