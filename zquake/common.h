dd5eb46f7ac90ece207b18cacbf62735607f531d
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2002-01-03 23:31:39 +0000

    Add Q_snprintfz library replacement function. Unlike the WIN32 _snprintf, it guarantees the resulting string will be \0 terminated

--- zquake/common.h
+++ zquake/common.h
@@ -176,6 +176,7 @@ int	Q_atoi (char *str);
 float Q_atof (char *str);
 
 void Q_strncpyz (char *dest, char *src, size_t size);
+void Q_snprintfz (char *dest, size_t size, char *fmt, ...);
 
 //============================================================================
 
