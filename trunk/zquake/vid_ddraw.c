59465123e9f6cce5c1d4801b6ce4c2c81496d18a
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2002-01-12 21:59:51 +0000

    Add a prototype for AppActivate at the top of file

--- trunk/zquake/vid_ddraw.c
+++ trunk/zquake/vid_ddraw.c
@@ -26,6 +26,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "keys.h"
 #include "sound.h"
 
+
+void AppActivate (BOOL fActive, BOOL minimize);
+
 extern int		Minimized;
 
 HWND		mainwindow;
