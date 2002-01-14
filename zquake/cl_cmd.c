1690b3411dcf02298bd55f67ded79dd295fbb41e
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2002-01-14 21:00:02 +0000

    menu.h is only included where needed now

--- zquake/cl_cmd.c
+++ zquake/cl_cmd.c
@@ -20,6 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "quakedef.h"
 #include "winquake.h"
+#include "menu.h"
 #include "teamplay.h"
 #include "version.h"
 
