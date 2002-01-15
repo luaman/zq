1141ecf5d370d4162962b19306f908f9e6a716ca
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2002-01-15 10:52:10 +0000

    New function, COM_Shutdown. Will set logfile cvar to 0 and thus close the log file if it's open.

--- zquake/common.h
+++ zquake/common.h
@@ -177,11 +177,13 @@ char *COM_Parse (char *data);
 extern	int		com_argc;
 extern	char	**com_argv;
 
+void COM_Init (void);
+void COM_Shutdown (void);
+
+void COM_InitArgv (int argc, char **argv);
 int COM_CheckParm (char *parm);
 void COM_AddParm (char *parm);
 
-void COM_Init (void);
-void COM_InitArgv (int argc, char **argv);
 
 char *COM_SkipPath (char *pathname);
 void COM_StripExtension (char *in, char *out);
