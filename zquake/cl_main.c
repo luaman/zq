3016dd2321db08534b510863f1d36fca199fecec
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2002-01-15 03:26:55 +0000

    pmove.h is not needed here any more

--- zquake/cl_main.c
+++ zquake/cl_main.c
@@ -26,7 +26,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "input.h"
 #include "keys.h"
 #include "menu.h"
-#include "pmove.h"
 #include "sbar.h"
 #include "sound.h"
 #include "version.h"
