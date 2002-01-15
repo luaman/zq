15cab6e4c26d8eee45c94b29eee317db5f15bc40
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2002-01-15 11:21:10 +0000

    Move host defs from quakedef.h and qwsvdef.h to common.h

--- trunk/zquake/quakedef.h
+++ trunk/zquake/quakedef.h
@@ -77,49 +77,10 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 //=============================================================================
 
-// the host system specifies the base of the directory tree, the
-// command line parms passed to the program, and the amount of memory
-// available for the program to use
-
-typedef struct
-{
-	char	*basedir;
-	int		argc;
-	char	**argv;
-	void	*membase;
-	int		memsize;
-} quakeparms_t;
-
-
-//=============================================================================
-
-//
-// host
-//
-extern	quakeparms_t host_parms;
-
 extern	cvar_t		developer;
 
-extern	qboolean	host_initialized;	// true if into command execution
 extern	byte		*host_basepal;
 extern	byte		*host_colormap;
 extern	double		realtime;			// not bounded in any way, changed at
 										// start of every frame, never reset
-
-void Host_Init (quakeparms_t *parms);
-void Host_Shutdown(void);
-void Host_Error (char *error, ...);
-void Host_EndGame (char *message, ...);
-void Host_Frame (double time);
-void Host_Quit (void);
-
-void CL_Init ();
-void CL_Shutdown ();
-void CL_Frame (double time);
-void CL_Disconnect ();
-void Con_Print (char *txt);
-
-void SV_Init (void);
-void SV_Shutdown (char *finalmsg);
-void SV_Frame (double time);
 
