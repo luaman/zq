6ecaccdec2e360a876e1e2e8934efd2e00cc1eb4
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2002-02-01 21:17:47 +0000

    Move standard #includes from quakedef.h/qwsvdef.h to common.h

--- trunk/zquake/quakedef.h
+++ trunk/zquake/quakedef.h
@@ -19,20 +19,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 // quakedef.h -- primary header for client
 
-#define	QUAKE_GAME			// as opposed to utilities
 
 //define	PARANOID			// speed sapping error checking
-
-#ifdef _WIN32
-#pragma warning( disable : 4244 4127 4201 4214 4514 4305 4115 4018)
-#endif
-
-
-#include <math.h>
-#include <string.h>
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 #include "common.h"
 
