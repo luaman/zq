603378736cd49354fc06cbc7bdbc5c843836b0f9
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2002-02-06 14:25:29 +0000

    quakedef.h --> common.h

--- trunk/zquake/mathlib.c
+++ trunk/zquake/mathlib.c
@@ -19,8 +19,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 // mathlib.c -- math primitives
 
-#include <math.h>
-#include "quakedef.h"
+#include "common.h"
 
 void Sys_Error (char *error, ...);
 
