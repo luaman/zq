33a2965c53152ffda0cd30dcf9c7288dd47cce1e
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2002-05-06 18:04:49 +0000

    *** empty log message ***

--- zquake/host.c
+++ zquake/host.c
@@ -26,7 +26,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <setjmp.h>
 
 
-#ifndef		SERVERONLY
+#if !defined(CLIENTONLY) && !defined(SERVERONLY)
 qboolean	dedicated = false;
 #endif
 
