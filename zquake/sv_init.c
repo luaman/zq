d0bd642b58d3b071bffb206fc04e714ee2c42e01
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2002-09-27 23:02:11 +0000

    server_static_t --> serverPersistent_t

--- zquake/sv_init.c
+++ zquake/sv_init.c
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "qwsvdef.h"
 #include "crc.h"
 
-server_static_t	svs;				// persistent server info
+serverPersistent_t	svs;			// persistent server info
 server_t		sv;					// local server
 
 char	localmodels[MAX_MODELS][5];	// inline model names for precache
