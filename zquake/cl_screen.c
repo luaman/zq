0b680b3ec6b74c389a76708dbd20e8996db076a7
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2003-04-20 19:35:01 +0000

    New source file, cl_screen.c. I'm going to move there common code from screen.c and gl_screen.c and maybe get read of screen & gl_screen one day

---
+++ zquake/cl_screen.c
@@ -0,0 +1,82 @@
+/*
+Copyright (C) 1996-1997 Id Software, Inc.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+// screen.c -- master for refresh, status bar, console, chat, notify, etc
+
+#include "quakedef.h"
+#ifdef GLQUAKE
+#include "gl_local.h"
+#else
+#include "r_local.h"
+#endif
+#include "keys.h"
+#include "image.h"
+#include "menu.h"
+#include "sbar.h"
+#include "sound.h"
+#include <time.h>
+
+/*
+
+background clear
+rendering
+turtle/net/ram icons
+sbar
+centerprint / slow centerprint
+notify lines
+intermission / finale overlay
+loading plaque
+console
+menu
+
+required background clears
+required update regions
+
+
+syncronous draw mode or async
+One off screen buffer, with updates either copied or xblited
+Need to double buffer?
+
+
+async draw will require the refresh area to be cleared, because it will be
+xblited, but sync draw can just ignore it.
+
+sync
+draw
+
+CenterPrint ()
+SlowPrint ()
+Screen_Update ();
+Com_Printf ();
+
+net 
+turn off messages option
+
+the refresh is always rendered, unless the console is full screen
+
+
+console is:
+	notify lines
+	half
+	full
+	
+
+*/
+
+
