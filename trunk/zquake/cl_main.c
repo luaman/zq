bc85e77f8dc27b8b09f18643c4b6fc98efabd2ff
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2003-06-24 07:24:59 +0000

    Add cl_fakename cvar

--- trunk/zquake/cl_main.c
+++ trunk/zquake/cl_main.c
@@ -71,6 +71,7 @@ cvar_t	cl_oldPL = {"cl_oldPL", "0"};
 cvar_t	cl_demoPingInterval = {"cl_demoPingInterval", "5"};
 cvar_t	cl_chatsound = {"cl_chatsound", "1"};
 cvar_t	cl_confirmquit = {"cl_confirmquit", "1", CVAR_INIT};
+cvar_t	cl_fakename = {"cl_fakename", ""};
 cvar_t	default_fov = {"default_fov", "0"};
 cvar_t	qizmo_dir = {"qizmo_dir", "qizmo"};
 
@@ -721,6 +722,7 @@ void CL_InitLocal (void)
 	Cvar_Register (&cl_demoPingInterval);
 	Cvar_Register (&cl_chatsound);
 	Cvar_Register (&cl_confirmquit);
+	Cvar_Register (&cl_fakename);
 	Cvar_Register (&default_fov);
 	Cvar_Register (&qizmo_dir);
 
