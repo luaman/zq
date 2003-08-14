4c15277d268021e46c6b179efd271904e7c2729d
digiman <digiman@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2003-08-14 23:51:31 +0000

    Fix ZQuake treating the ";" as separator for commands even in commented lines (patch supplied by Massa and fuh)

--- trunk/zquake/cmd.c
+++ trunk/zquake/cmd.c
@@ -167,28 +167,32 @@ Cbuf_Execute
 */
 void Cbuf_ExecuteEx (cbuf_t *cbuf)
 {
-	int		i, j;
+	int		i, j, cursize;
 	char	*text;
 	char	line[1024], *src, *dest;
-	int		quotes;
-	int		cursize;
+	qboolean comment, quotes;
 
 	cbuf_current = cbuf;
 
 	while (cbuf->text_end > cbuf->text_start)
 	{
-// find a \n or ; line break
+		// find a \n or ; line break
 		text = (char *)cbuf->text_buf + cbuf->text_start;
 
 		cursize = cbuf->text_end - cbuf->text_start;
-		quotes = 0;
-		for (i=0 ; i<cursize ; i++)
+		comment = quotes = false;
+		for (i = 0; i < cursize; i++)
 		{
-			if (text[i] == '"')
-				quotes++;
-			if ( !(quotes&1) &&  text[i] == ';')
-				break;	// don't break if inside a quoted string
 			if (text[i] == '\n')
+				break;
+			if (text[i] == '"')
+				quotes = !quotes;
+			if (comment || quotes)
+				continue;
+
+			if (text[i] == '/' && (i + 1) < cursize && text[i + 1] == '/')
+				comment = true;
+			else if (text[i] == ';')
 				break;
 		}
 
