3234dad7b37f56fb63775692ec871bab15202ced
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2003-10-09 10:13:44 +0000

    Move mplane_t and hull_t out of *_model.h and into cmodel.h. This doesn't mean we have a Q2-ish cmodel now (not yet), but lets us get rid of quakedef.h in pmove.c :-)

---
+++ trunk/zquake/cmodel.h
@@ -0,0 +1,34 @@
+// cmodel.h
+
+#include "bspfile.h"
+
+// mplane->type
+enum {
+	SIDE_FRONT = 0,
+	SIDE_BACK = 1,
+	SIDE_ON = 2
+};
+
+
+// plane_t structure
+// !!! if this is changed, it must be changed in asm_i386.h too !!!
+typedef struct mplane_s
+{
+	vec3_t	normal;
+	float	dist;
+	byte	type;			// for texture axis selection and fast side tests
+	byte	signbits;		// signx + signy<<1 + signz<<1
+	byte	pad[2];
+} mplane_t;
+
+// !!! if this is changed, it must be changed in asm_i386.h too !!!
+typedef struct
+{
+	dclipnode_t	*clipnodes;
+	mplane_t	*planes;
+	int			firstclipnode;
+	int			lastclipnode;
+	vec3_t		clip_mins;
+	vec3_t		clip_maxs;
+} hull_t;
+
