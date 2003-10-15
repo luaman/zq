5d43c584ff04bb387a590b2b17eafc9b2b50a61b
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2003-10-15 20:27:41 +0000

    Don't need pmove.h here

--- trunk/zquake/sv_main.c
+++ trunk/zquake/sv_main.c
@@ -19,7 +19,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include "qwsvdef.h"
-#include "pmove.h"
 #include "version.h"
 
 int			current_skill;			// for entity spawnflags checking
