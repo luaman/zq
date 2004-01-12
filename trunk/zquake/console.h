fd0f779d1186dd059e414da2092220b66c45dda4
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2004-01-12 04:22:17 +0000

    Increase CON_TEXTSIZE to 64 KB

--- trunk/zquake/console.h
+++ trunk/zquake/console.h
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // console
 //
 
-#define		CON_TEXTSIZE	32768
+#define		CON_TEXTSIZE	65536
 typedef struct
 {
 	char	text[CON_TEXTSIZE];
