43b062a2ce0b1e67fc1e78cc95f70729f633d3e4
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2004-01-15 15:31:44 +0000

    Move PHS stuff to cmodel

--- trunk/zquake/sv_send.c
+++ trunk/zquake/sv_send.c
@@ -203,17 +203,10 @@ void SV_Multicast (vec3_t origin, int to)
 {
 	client_t	*client;
 	byte		*mask;
-	mleaf_t		*leaf;
 	int			leafnum;
 	int			j;
 	qboolean	reliable;
-	vec3_t		org;
-
-	leaf = Mod_PointInLeaf (origin, sv.FIXME_worldmodel);
-	if (!leaf)
-		leafnum = 0;
-	else
-		leafnum = leaf - sv.FIXME_worldmodel->leafs;
+	vec3_t		vieworg;
 
 	reliable = false;
 
@@ -222,46 +215,49 @@ void SV_Multicast (vec3_t origin, int to)
 	case MULTICAST_ALL_R:
 		reliable = true;	// intentional fallthrough
 	case MULTICAST_ALL:
-		mask = sv.pvs;		// leaf 0 is everything;
+		mask = NULL;		// everything
 		break;
 
 	case MULTICAST_PHS_R:
 		reliable = true;	// intentional fallthrough
 	case MULTICAST_PHS:
-		mask = sv.phs + leafnum * 4*((sv.FIXME_worldmodel->numleafs+31)>>5);
+		mask = CM_LeafPHS (CM_PointInLeaf(origin));
 		break;
 
 	case MULTICAST_PVS_R:
 		reliable = true;	// intentional fallthrough
 	case MULTICAST_PVS:
-		mask = sv.pvs + leafnum * 4*((sv.FIXME_worldmodel->numleafs+31)>>5);
+		mask = CM_LeafPVS (CM_PointInLeaf (origin));
 		break;
 
 	default:
 		mask = NULL;
-		Host_Error ("SV_Multicast: bad to:%i", to);
+		Host_Error ("SV_Multicast: bad to: %i", to);
 	}
 
-	// send the data to all relevent clients
+	// send the data to all relevant clients
 	for (j = 0, client = svs.clients; j < MAX_CLIENTS; j++, client++)
 	{
 		if (client->state != cs_spawned)
 			continue;
 
-		VectorAdd (client->edict->v.origin, client->edict->v.view_ofs, org);
+		if (!mask)
+			goto inrange;	// multicast to all
+
+		VectorAdd (client->edict->v.origin, client->edict->v.view_ofs, vieworg);
 
 		if (to == MULTICAST_PHS_R || to == MULTICAST_PHS) {
 			vec3_t delta;
-			VectorSubtract(origin, org, delta);
+			VectorSubtract(origin, vieworg, delta);
 			if (VectorLength(delta) <= 1024)
 				goto inrange;
 		}
 
-		leaf = Mod_PointInLeaf (org, sv.FIXME_worldmodel);
-		if (leaf)
+		leafnum = CM_Leafnum(CM_PointInLeaf(vieworg));
+		if (leafnum)
 		{
 			// -1 is because pvs rows are 1 based, not 0 based like leafs
-			leafnum = leaf - sv.FIXME_worldmodel->leafs - 1;
+			leafnum = leafnum - 1;
 			if ( !(mask[leafnum>>3] & (1<<(leafnum&7)) ) )
 			{
 //				Com_Printf ("supressed multicast\n");
