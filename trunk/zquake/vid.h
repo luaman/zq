37ae884649d29fe664054d53d81113110edc1734
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2004-01-20 02:10:48 +0000

    Make vid.rowbytes, vid.width and vid.height ints.  Carmack must have been on drugs when he made them unsigned

--- trunk/zquake/vid.h
+++ trunk/zquake/vid.h
@@ -37,10 +37,10 @@ typedef struct
 	pixel_t			*colormap;		// 256 * VID_GRADES size
 	unsigned short	*colormap16;	// 256 * VID_GRADES size
 	int				fullbright;		// index of first fullbright color
-	unsigned		rowbytes;	// may be > width if displayed in a window
-	unsigned		width;		
-	unsigned		height;
-	float			aspect;		// width / height -- < 0 is taller than wide
+	int				rowbytes;		// may be > width if displayed in a window
+	int				width;		
+	int				height;
+	float			aspect;			// width / height -- < 0 is taller than wide
 	int				numpages;
 	int				recalc_refdef;	// if true, recalc vid-based stuff
 	int				realwidth;		// pixel width of GL window
