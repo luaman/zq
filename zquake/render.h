d6c59c1574a548384ef45a85c147e0d6bb137474
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2004-05-26 01:02:05 +0000

    Increase default MAX_PARTICLES to 4096

--- zquake/render.h
+++ zquake/render.h
@@ -24,7 +24,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define	BOTTOM_RANGE	96
 
 #define	MAX_VISEDICTS			256
-#define MAX_PARTICLES			2048	// default max # of particles at one
+#define MAX_PARTICLES			4096	// default max # of particles at one
 										// time
 
 // entity->renderfx
