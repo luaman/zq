bf0837df43c74227532ba4a6d71f90f997d8d702
oldmanuk <oldmanuk@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2004-06-24 12:02:24 +0000

    Change this to DMA stubs

--- zquake/snd_osx.c
+++ zquake/snd_osx.c
@@ -18,93 +18,47 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <stdio.h>
+
 #include "quakedef.h"
 #include "sound.h"
 
-channel_t   channels[MAX_CHANNELS];
-int			total_channels;
+int audio_fd;
+int snd_inited;
 
-qbool		snd_initialized = false;
+static int tryrates[] = { 11025, 22051, 44100, 8000 };
 
-dma_t		dma; 
-
-int   		paintedtime; 	// sample PAIRS
-
-cvar_t bgmvolume = {"bgmvolume", "1", true};
-cvar_t s_volume = {"s_volume", "0.7", true};
-cvar_t s_loadas8bit = {"s_loadas8bit", "0"};
-cvar_t s_swapstereo = {"s_swapstereo", "0", CVAR_ARCHIVE};
-
-void S_Restart (void)
+qbool SNDDMA_Init(void)
 {
+
 }
- 
-void S_Init (void)
+
+int SNDDMA_GetDMAPos(void)
 {
+
 }
 
-void S_AmbientOff (void)
+void SNDDMA_Shutdown(void)
 {
+
 }
 
-void S_AmbientOn (void)
-{
-}
+/*
+==============
+SNDDMA_Submit
 
-void S_Shutdown (void)
-{
-}
-
-void S_TouchSound (char *sample)
-{
-}
-
-void S_ClearBuffer (void)
-{
-}
-
-void S_StaticSound (sfx_t *sfx, vec3_t origin, float vol, float attenuation)
-{
-}
-
-void S_StartSound (int entnum, int entchannel, sfx_t *sfx, vec3_t origin, float fvol,  float attenuation)
-{
-}
-
-void S_StopSound (int entnum, int entchannel)
-{
-}
-
-sfx_t *S_PrecacheSound (char *sample)
-{
-	return NULL;
-}
-
-void S_ClearPrecache (void)
-{
-}
-
-void S_Update (vec3_t origin, vec3_t v_forward, vec3_t v_right, vec3_t v_up)
-{	
-}
-
-void S_StopAllSounds (qbool clear)
-{
-}
-
-void S_BeginPrecaching (void)
-{
-}
-
-void S_EndPrecaching (void)
-{
-}
-
-void S_ExtraUpdate (void)
-{
-}
-
-void S_LocalSound (char *s)
+Send sound to device if buffer isn't really the dma buffer
+===============
+*/
+void SNDDMA_Submit(void)
 {
 }
 
