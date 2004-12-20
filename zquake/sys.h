e7bdf7be4b73263a5db828152b1683c3c1880fb9
blimpyboy <blimpyboy@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2004-12-20 16:04:47 +0000

    When there is actually a need for Sys_FileTime(), you gimme a call and I'll re-add it.

--- zquake/sys.h
+++ zquake/sys.h
@@ -25,7 +25,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // file IO
 //
 
-int	Sys_FileTime (char *path);
 void Sys_mkdir (char *path);
 
 //
--- zquake/sys_linux.c
+++ zquake/sys_linux.c
@@ -114,23 +114,6 @@ void		 Sys_Error (char *error, ...)
 }
 
 
-/*
-============
-Sys_FileTime
-
-returns -1 if not present
-============
-*/
-int			 Sys_FileTime (char *path)
-{
-	struct stat	 buf;
-
-	if (stat (path,&buf) == -1)
-		return -1;
-
-	return buf.st_mtime;
-}
-
 
 void		 Sys_mkdir (char *path)
 {
