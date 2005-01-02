4aee6c88b94badc190a77510936c3c030f54fb37
matatk <matatk@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2005-01-02 20:54:50 +0000

    fix when using ALSA: ignore BIGENDIAN check.

--- zquake/snd_mem.c
+++ zquake/snd_mem.c
@@ -134,6 +134,7 @@ sfxcache_t *S_LoadSound (sfx_t *s)
 	if (!sc)
 		return NULL;
 
+#ifndef ALSA
 #ifdef BIGENDIAN
 	if (info.width == 2) {
 		int i;
@@ -145,6 +146,7 @@ sfxcache_t *S_LoadSound (sfx_t *s)
 		for (i = 0; i < len; i++)
 			ptr[i] = LittleShort(ptr[i]);
 	}
+#endif
 #endif
 
 	sc->length = info.samples;
