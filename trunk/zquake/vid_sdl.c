62f998d267359220b6e5c54b6ac8ea1a75015f1e
oldmanuk <oldmanuk@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2005-03-22 18:43:58 +0000

    Only use fullscreenhack on OSX as it cant do 320x200 but X11 etc. can

--- trunk/zquake/vid_sdl.c
+++ trunk/zquake/vid_sdl.c
@@ -17,7 +17,9 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
+#ifdef __APPLE__
 #define FULLSCREENHACK
+#endif
 
 #include <SDL/SDL.h>
 #include "quakedef.h"
