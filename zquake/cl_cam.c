449ceb66cce531acc1dcc0c448e6822d19e567ee
oldmanuk <oldmanuk@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2005-04-15 15:30:36 +0000

    Accidentally commited without comment last time. Trying to fix up skin and colour forcing while watching demos

--- zquake/cl_cam.c
+++ zquake/cl_cam.c
@@ -22,6 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "quakedef.h"
 #include "pmove.h"
 #include "cl_sbar.h"
+#include "teamplay.h"
 
 
 qbool		cam_track;
