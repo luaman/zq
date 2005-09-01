cb96b1dd5d849399b17b817954c9914dea45318b
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2005-09-01 20:27:30 +0000

    oops

--- trunk/zquake/cl_tent.c
+++ trunk/zquake/cl_tent.c
@@ -175,7 +175,7 @@ void CL_ParseBeam (int type)
 	// an experimental protocol extension:
 	// TE_LIGHTNING1 with entity num in -512..-1 range is a rail trail
 	if (type == 1 && (ent >= -512 && ent <= -1)) {
-		char colors[8] = { 6 /* white (change to something else?) */,
+		int colors[8] = { 6 /* white (change to something else?) */,
 			208 /* blue */,
 			180 /* green */, 35 /* light blue */, 224 /* red */,
 			133 /* magenta... kinda */, 192 /* yellow */, 6 /* white */};
@@ -185,7 +185,7 @@ void CL_ParseBeam (int type)
 		// -512..-257 are colored trails assigned to a specific
 		// player, so overrides can be applied; encoded as follows:
 		// 7654321076543210
-		// 1111111nnnnnnссс  (n = player num, c = color code)
+		// 1111111nnnnnnccc  (n = player num, c = color code)
 		cnum = ent & 7;
 		pnum = (ent >> 3) & 63;
 		if (pnum < MAX_CLIENTS)
