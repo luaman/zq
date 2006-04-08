8e32bb041e333b4f33d6bb441c18f997fc872906
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2006-04-08 17:33:32 +0000

    Add 'weapon' command (it's an alias for 'implulse')

--- zquake/cl_input.c
+++ zquake/cl_input.c
@@ -761,6 +761,7 @@ void CL_InitInput (void)
 	Cmd_AddCommand ("+jump", IN_JumpDown);
 	Cmd_AddCommand ("-jump", IN_JumpUp);
 	Cmd_AddCommand ("impulse", IN_Impulse);
+	Cmd_AddCommand ("weapon", IN_Impulse);
 	Cmd_AddCommand ("+klook", IN_KLookDown);
 	Cmd_AddCommand ("-klook", IN_KLookUp);
 	Cmd_AddCommand ("+mlook", IN_MLookDown);
