381ecedd9040badb8697eea57ef219f73aab2169
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2006-09-10 22:26:57 +0000

    shut up warning on OS X / Xcode

--- trunk/zquake/net_chan.c
+++ trunk/zquake/net_chan.c
@@ -20,7 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "common.h"
 
-#if defined(__linux__) || defined(sun) || defined(darwin) || defined(hpux) || defined(__OpenBSD__)
+#if defined(__linux__) || defined(sun) || defined(darwin) || defined(__APPLE__) || defined(hpux) || defined(__OpenBSD__)
 #include <sys/stat.h>
 #include <unistd.h>
 #include <errno.h>
