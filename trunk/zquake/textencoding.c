6b63a127b6f2c978fd8f202bc3c809c771a2a464
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2007-01-15 21:40:22 +0000

    1. CP1251 encoding is now understood by decode_string() 2. decode_string will transliterate cyrillic text if the cyrillic charset is not available

--- trunk/zquake/textencoding.c
+++ trunk/zquake/textencoding.c
@@ -1,6 +1,8 @@
 #include "q_shared.h"
 #include "textencoding.h"
 
+extern qbool R_CharAvailable (wchar c);
+
 /* KOI8-R encodes Russian capital hard sign as 0xFF, but we can't use it
 because it breaks older clients (qwcl).  We use 0xaf ('/'+ 0x80) instead. */
 static char *wc2koi_table =
@@ -69,17 +71,17 @@ static wchar koi2wc (char c)
 	else if (uc == '3' + 128)
 		return 0x0401;	// russian capital yo
 	else if (uc == '4' + 128)
-		return 0x0404;	// ukranian capital round E
+		return 0x0404;	// ukrainian capital round E
 	else if (uc == '$' + 128)
-		return 0x0454;	// ukranian small round E
+		return 0x0454;	// ukrainian small round E
 	else if (uc == '6' + 128)
-		return 0x0406;	// ukranian capital I
+		return 0x0406;	// ukrainian capital I
 	else if (uc == '&' + 128)
-		return 0x0456;	// ukranian small i
+		return 0x0456;	// ukrainian small i
 	else if (uc == '7' + 128)
-		return 0x0407;	// ukranian capital I with two dots
+		return 0x0407;	// ukrainian capital I with two dots
 	else if (uc == '\'' + 128)
-		return 0x0457;	// ukranian small i with two dots
+		return 0x0457;	// ukrainian small i with two dots
 	else if (uc == '>' + 128)
 		return 0x040e;	// belarusian Y
 	else if (uc == '.' + 128)
@@ -90,8 +92,35 @@ static wchar koi2wc (char c)
 		return (wchar)(unsigned char)c;
 }
 
+static wchar cp1251towc (char c)
+{
+	unsigned char uc = c;
+	if (uc >= 192)
+		return 0x0410 + (uc - 192);
+	else if (uc == 168)
+		return 0x0401;	// russian capital yo
+	else if (uc == 184)
+		return 0x0451;	// russian small yo
+	else if (uc == 170)
+		return 0x0404;	// ukrainian capital round E
+	else if (uc == 186)
+		return 0x0454;	// ukrainian small round E
+	else if (uc == 178)
+		return 0x0406;	// ukrainian capital I
+	else if (uc == 179)
+		return 0x0456;	// ukrainian small i
+	else if (uc == 175)
+		return 0x0407;	// ukrainian capital I with two dots
+	else if (uc == 191)
+		return 0x0457;	// ukrainian small i with two dots
+	else if (uc == 161)
+		return 0x040e;	// belarusian Y
+	else if (uc == 162)
+		return 0x045e;	// belarusian y
+	return (wchar)uc;
+}
 
-wchar *decode_string (const char *s)
+wchar *decode_stringOLD (const char *s)
 {
 	static wchar buf[2048];	// should be enough for everyone!!!
 
@@ -102,7 +131,7 @@ wchar *decode_string (const char *s)
 		char *p;
 		wchar *out = buf;
 
-Com_DPrintf ("%s\n", s);
+//Com_DPrintf ("%s\n", s);
 		p = strstr(s, "=`koi8q:");
 		for (i = 0; i < p - s; i++)
 			*out++ = char2wc(s[i]);
@@ -118,8 +147,193 @@ Com_DPrintf ("%s\n", s);
 				*out++ = *p++;
 		}
 		*out++ = 0;
-		return buf;
+		return maybe_transliterate(buf);
 	}
 	return str2wcs(s);
 }
 
+// returns Q_malloc'ed data
+wchar *decode_koi8q (char *str) {
+	wchar *buf, *out;
+	buf = out = Q_malloc ((strlen(str) + 1)*sizeof(wchar));
+	while (*str)
+		*out++ = koi2wc(*str++);
+	*out = 0;
+	return buf;
+};
+
+// returns Q_malloc'ed data
+wchar *decode_cp1251 (char *str) {
+	wchar *buf, *out;
+	buf = out = Q_malloc ((strlen(str) + 1)*sizeof(wchar));
+	while (*str)
+		*out++ = cp1251towc(*str++);
+	*out = 0;
+	return buf;
+};
+
+typedef wchar *(*decodeFUNC) (char *);
+
+static struct {
+	char *name;
+	decodeFUNC func;
+} decode_table[] = {
+	{"koi8q", decode_koi8q},
+	{"koi8r", decode_koi8q},
+	{"k8", decode_koi8q},
+	{"cp1251", decode_cp1251},
+	{"wr", decode_cp1251},	// wc = Windows Cyrillic wr = Windows Russian
+	{NULL, NULL}
+};
+
+wchar *decode_string (const char *s)
+{
+	static wchar buf[1024];	// should be enough for everyone!!!
+	char encoding[13];
+	char enc_str[1024];
+	const char *p, *q, *r;
+	wchar *decoded;
+	int i;
+
+	buf[0] = 0;
+	p = s;
+
+	while (1)
+	{
+		p = strstr(p, "=`");
+		if (!p)
+			break;
+
+		// copy source string up to p as is
+		qwcslcat (buf, str2wcs(s), min(p-s+qwcslen(buf)+1, sizeof(buf)/sizeof(buf[0])));
+		s = p;
+
+		p += 2;		// skip the =`
+		for (q = p; isalnum(*q) && q - p < 12; q++)	{
+			;
+		}
+		if (!*q)
+			break;
+		if (*q != ':') {
+			p += 2;
+			continue;
+		}
+
+		q++;	// skip the :
+		assert (q - p <= sizeof(encoding));
+		strlcpy (encoding, p, q - p);
+		
+		r = strstr(q, "`=");
+		if (!r) {
+			p = q;
+			continue;
+		}
+
+		strlcpy (enc_str, q, min(r - q + 1, sizeof(enc_str)));
+
+		for (i = 0; decode_table[i].name; i++) {
+			if (!Q_stricmp(encoding, decode_table[i].name))
+				break;	// found it
+		}
+
+		if (!decode_table[i].name) {
+			// unknown encoding
+			p = r + 2;
+			continue;	
+		}
+
+		decoded = decode_table[i].func(enc_str);
+		qwcslcat (buf, decoded, sizeof(buf)/sizeof(buf[0]));
+		Q_free (decoded);
+		s = p = r + 2;
+	}
+
+	// copy remainder as is
+	qwcslcat (buf, str2wcs(s), sizeof(buf)/sizeof(buf[0]));
+	return buf;
+}
+
+
+char cyr_trans_table[] = {"ABVGDEZZIJKLMNOPRSTUFHCCSS'Y'EYY"};
+wchar *transliterate_char (wchar c)
+{
+	static wchar s[2] = {0};
+
+	if (c == 0x401)
+		return str2wcs("YO");
+	if (c == 0x404)
+		return str2wcs("E");
+	if (c == 0x406)
+		return str2wcs("I");
+	if (c == 0x407)
+		return str2wcs("J");
+	if (c == 0x40e)
+		return str2wcs("U");
+	if (c == 0x416)
+		return str2wcs("ZH");
+	if (c == 0x427)
+		return str2wcs("CH");
+	if (c == 0x428)
+		return str2wcs("SH");
+	if (c == 0x429)
+		return str2wcs("SH");
+	if (c == 0x42e)
+		return str2wcs("YU");
+	if (c == 0x42f)
+		return str2wcs("YA");
+	if (c == 0x451)
+		return str2wcs("yo");
+	if (c == 0x454)
+		return str2wcs("e");
+	if (c == 0x456)
+		return str2wcs("i");
+	if (c == 0x457)
+		return str2wcs("j");
+	if (c == 0x45e)
+		return str2wcs("u");
+	if (c == 0x436)
+		return str2wcs("zh");
+	if (c == 0x447)
+		return str2wcs("ch");
+	if (c == 0x448)
+		return str2wcs("sh");
+	if (c == 0x449)
+		return str2wcs("sh");
+	if (c == 0x44e)
+		return str2wcs("yu");
+	if (c == 0x44f)
+		return str2wcs("ya");
+	if (c >= 0x0410 && c < 0x0430)
+		s[0] = cyr_trans_table[c - 0x410];
+	else if (c >= 0x0430 && c < 0x0450)
+		s[0] = tolower(cyr_trans_table[c - 0x430]);
+	else
+		s[0] = '?';
+	return s;
+}
+
+// Make sure the renderer can display all Unicode chars in the string,
+// otherwise try to replace them with Latin equivalents
+wchar *maybe_transliterate (wchar *src)
+{
+	wchar *dst, *trans;
+	static wchar buf[2048];
+#define buflen (sizeof(buf)/sizeof(buf[0]))
+	int len;
+
+	dst = buf;
+	while (*src && dst < buf+buflen-1) {
+		if (R_CharAvailable(*src))
+			*dst++ = *src;
+		else {
+			trans = transliterate_char (*src);
+			len = min(qwcslen(trans), buf+buflen-1 - dst);
+			memcpy (dst, trans, len*sizeof(wchar));
+			dst += len;
+		}
+		src++;
+	}
+	*dst = 0;
+
+	return buf;
+}
