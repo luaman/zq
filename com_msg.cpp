d489d6ba763dce0532d4a46b9fb1a1b04f786e2a
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2007-03-24 07:31:33 +0000

    MSG_WriteString now accepts a std::string

--- com_msg.cpp
+++ com_msg.cpp
@@ -99,12 +99,9 @@ void MSG_WriteFloat (sizebuf_t *sb, float f)
 	SZ_Write (sb, &dat.l, 4);
 }
 
-void MSG_WriteString (sizebuf_t *sb, char *s)
+void MSG_WriteString (sizebuf_t *sb, const string s)
 {
-	if (!s)
-		SZ_Write (sb, "", 1);
-	else
-		SZ_Write (sb, s, strlen(s)+1);
+	SZ_Write (sb, s.c_str(), strlen(s.c_str())+1);
 }
 
 void MSG_WriteCoord (sizebuf_t *sb, float f)
