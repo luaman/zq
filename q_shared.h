fa05a25e090f1dd0b88b9f7f7e278014c254b8f2
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2007-10-03 09:32:39 +0000

    Move ON_EPSILON to gl_sky.cpp, the only place it's ever used

--- q_shared.h
+++ q_shared.h
@@ -100,9 +100,6 @@ typedef int qbool;
 #define	MAX_QPATH		64			// max length of a quake game pathname
 #define	MAX_OSPATH		128			// max length of a filesystem pathname
 
-#define	ON_EPSILON		0.1			// point on plane side epsilon
-
-
 //============================================================================
 
 typedef struct sizebuf_s
