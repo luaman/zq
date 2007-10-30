a88b6f3fe20476b02a57017cd89e144df5570314
tonik <tonik@3bd3e6fb-f625-0410-8400-cbb201654aa1>
2007-10-30 20:08:48 +0000

    Add Sys_MSleep macro

--- zquake/sys.h
+++ zquake/sys.h
@@ -21,6 +21,12 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef _SYS_H_
 #define _SYS_H_
 
+#ifdef _WIN32
+#define Sys_MSleep(x) Sleep(x)
+#else
+#define Sys_MSleep(x) usleep((x) * 1000)
+#endif
+
 //
 // file IO
 //
